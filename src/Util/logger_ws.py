85ec8accaf05faba891dedf5696acb2255498a4d
andres <andreslamosk124@hotmail.com>
2025-12-07 20:14:53 -0600

    Remove IntelliJ `.idea` configuration files and sensitive hardcoded tokens for cleaner repository; replace with environment variables in `logger_ws.py` for secure token management.

--- src/Util/logger_ws.py
+++ src/Util/logger_ws.py
@@ -1,12 +1,16 @@
+import os
+
 import aiohttp
+
+LOG_TOKEN_USER = os.environ.get("LOG_TOKEN_USER")
+LOG_TOKEN_REALM = os.environ.get("LOG_TOKEN_REALM")
 
 
 async def logger(payload: dict, realm: str, collection: str):
     url = 'https://log.arz.ai'
-    # url = 'http://127.0.0.1:8000'
     headers = {
-        'X-token-user': '9D8B161E8870DE18203E767F81445CB2CA5A2DA6E997573635DD25F290BAB59E',
-        'X-token-realm': '9CFE23E08EA0DCAAAEA5AA642F3BF7A4A38EBB2F05C053CD0363A8FD87995A41'
+        'X-token-user': LOG_TOKEN_USER,
+        'X-token-realm': LOG_TOKEN_REALM
     }
     async with aiohttp.ClientSession() as session:
         async with session.put(url + f'/log/{realm}/{collection}',
